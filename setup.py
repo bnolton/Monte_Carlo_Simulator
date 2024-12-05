dc0eff89295ae9f244f2a6ddb5c28611ac59938a
bnolton <brian.nolton@outlook.com>
2024-12-05 10:33:03 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Monte_Carlo_Simulator',
+    version='0.1',
+    description='This a Monte Carlo Simulator for the final for the DS5100 class at UVA',
+    url='http://github.com/bnolton/Monte_Carlo_Simulator',
+    author='Brian Nolton',
+    author_email='frv3fp@virginia.edu',
+    license='MIT',
+    packages=['Monte_Carlo_Simulator']
+)
